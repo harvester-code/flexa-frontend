e5f22d16321cd70989abf4595d13c067b5086e98
YI <yibyungha@gmail.com>
2025-02-17 19:10:58 +0900

    ✨ Add submit-button component

---
+++ src/components/SubmitButton.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { type ComponentProps } from 'react';
+import { useFormStatus } from 'react-dom';
+import { Button } from '@/components/ui/button';
+
+type Props = ComponentProps<typeof Button> & {
+  pendingText?: string;
+};
+
+export function SubmitButton({ children, pendingText = 'Submitting...', ...props }: Props) {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button className="btn-lg btn-gradient mt-25" type="submit" aria-disabled={pending} {...props}>
+      {pending ? pendingText : children}
+    </Button>
+  );
+}
