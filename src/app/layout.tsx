117300f71662f7dd259f3ded16fcea5c35db789c
Vincent <vincent@datamarketing.co.kr>
2025-03-06 17:56:23 +0900

    🎨 Improve format of codes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,11 +19,8 @@ export const metadata: Metadata = {
   description: 'Make your airport wait-free',
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+// FIXME: 레이아웃 통일하기
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <ClientProviders>
