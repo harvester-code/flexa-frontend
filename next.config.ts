073d57c7099f2e54ae1c0c8ed490cdecbc7acf30
Vincent <vincent@datamarketing.co.kr>
2025-04-05 21:46:09 +0900

    ♻️ Update Bitbucket pipeline and Dockerfile

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'picsum.photos' },
       { protocol: 'https', hostname: process.env.NEXT_PUBLIC_SUPABASE_URL?.split('//')[1] || '' },
     ],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: 'standalone',
 };
 
 export default nextConfig;
