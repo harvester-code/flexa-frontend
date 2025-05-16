d4c4e6bcd24dcaf21c657997bdec7c4a68c9f415
Vincent <vincent@datamarketing.co.kr>
2025-05-16 18:37:23 +0900

    ♻️ Refactor TheHistogramChart component into HomeCharts

--- src/app/(protected)/home/_components/HomeCharts.tsx
+++ src/app/(protected)/home/_components/HomeCharts.tsx
@@ -9,6 +9,7 @@ import { useHistogramChart, useLineChart, useSankeyChart } from '@/queries/homeQ
 import { PRIMARY_COLOR_SCALES, SANKEY_COLOR_SCALES } from '@/constants';
 import Checkbox from '@/components/Checkbox';
 import TheDropdownMenu from '@/components/TheDropdownMenu';
+import TheHistogramChart from '@/components/charts/TheHistogramChart';
 import { Button, ButtonGroup } from '@/components/ui/Button';
 import { cn } from '@/lib/utils';
 import HomeLoading from './HomeLoading';
@@ -277,9 +278,7 @@ function HomeCharts({ scenario, processes }: HomeChartsProps) {
                     key={i}
                     onClick={() => handleChartOption1(i)}
                   >
-                    {chartOption1.includes(i) && (
-                      <Circle className="!size-2.5" fill="#111" stroke="transparent" />
-                    )}
+                    {chartOption1.includes(i) && <Circle className="!size-2.5" fill="#111" stroke="transparent" />}
                     {opt.label}
                   </Button>
                 ))}
@@ -331,30 +330,7 @@ function HomeCharts({ scenario, processes }: HomeChartsProps) {
             </div>
           </div>
 
-          <div className="mt-10 rounded-md bg-white">
-            <div className="flex rounded-lg text-center">
-              {histogramChartData &&
-                histogramChartData?.map(({ title, value, color }, idx) => (
-                  <div style={{ width: `${value}%` }} key={idx}>
-                    <div
-                      className={`py-3.5 ${
-                        histogramChartData.length === 1
-                          ? 'rounded-lg'
-                          : idx === 0
-                            ? 'rounded-l-lg'
-                            : idx === histogramChartData.length - 1
-                              ? 'rounded-r-lg'
-                              : ''
-                      }`}
-                      style={{ background: `${color}` }}
-                    >
-                      <p className="text-3xl font-bold text-white">{value}%</p>
-                    </div>
-                    <p className="mt-1 text-sm font-medium">{title}</p>
-                  </div>
-                ))}
-            </div>
-          </div>
+          <TheHistogramChart chartData={histogramChartData} />
         </div>
       </div>
 
