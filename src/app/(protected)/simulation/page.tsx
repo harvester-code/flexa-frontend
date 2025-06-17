35a0afd15f3288029f5a4ff46942cc6792676bc6
Vincent <vincent@datamarketing.co.kr>
2025-06-17 10:52:28 +0900

    ♻️ Refactor simulation page component

--- src/app/(protected)/simulation/page.tsx
+++ src/app/(protected)/simulation/page.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
 import { useQueryClient } from '@tanstack/react-query';
 import dayjs from 'dayjs';
-import { EllipsisVertical, ListFilterIcon, Trash2 } from 'lucide-react';
+import { EllipsisVertical, Trash2 } from 'lucide-react';
 import { OrbitProgress } from 'react-loading-indicators';
 import { deleteScenario, duplicateScenario, modifyScenario, setMasterScenario } from '@/services/simulations';
 import { useScenarios } from '@/queries/simulationQueries';
@@ -13,7 +13,6 @@ import { useUser } from '@/queries/userQueries';
 import Button from '@/components/Button';
 import Checkbox from '@/components/Checkbox';
 import Paging from '@/components/Paging';
-import Search from '@/components/Search';
 import TheContentHeader from '@/components/TheContentHeader';
 import { PushCreateScenarioPopup } from '@/components/popups/CreateScenario';
 import { PopupAlert } from '@/components/popups/PopupAlert';
@@ -24,8 +23,9 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/DropdownMenu';
+import { cn } from '@/lib/utils';
 
-interface IScenarioStates {
+interface ScenarioStates {
   name: string;
   memo: string;
   editName: boolean;
@@ -36,50 +36,44 @@ interface IScenarioStates {
 
 const PAGE_ROW_COUNT = 11;
 
-const SimulationPage: React.FC = () => {
+const SimulationPage = () => {
   const queryClient = useQueryClient();
 
   const pathname = usePathname();
   const router = useRouter();
 
-  const initialVisibleDiv: 'tag' | 'full' = 'tag';
-  const [visibleDiv] = useState<'tag' | 'full'>(initialVisibleDiv);
-
   const [selectAll, setSelectAll] = useState(false);
-  const [selected, setSelected] = useState<boolean[]>([]);
+  const [isScenarioSelected, setIsScenarioSelected] = useState<boolean[]>([]);
   const [searchKeyword, setSearchKeyword] = useState('');
 
-  const [anchorEls, setAnchorEls] = useState<(HTMLElement | null)[]>(Array(10).fill(null));
-  const [scenarioStates, setScenarioStates] = useState<IScenarioStates[]>([]);
+  const [scenarioStates, setScenarioStates] = useState<ScenarioStates[]>([]);
   const [page, setPage] = useState(1);
 
   const { data: userInfo } = useUser();
-  const { scenarios, totalCount } = useScenarios(userInfo?.groupId, page);
+  const { scenarios, totalCount, isLoading: isScenariosLoading } = useScenarios(userInfo?.groupId, page);
 
   useEffect(() => {
-    if (scenarios?.length > 0) {
-      setSelected(Array(scenarios?.length).fill(false));
-      setScenarioStates(
-        scenarios.map((item) => {
-          return {
-            name: item.name,
-            // name: item.simulation_name,
-            memo: item.memo,
-            editName: false,
-            editMemo: false,
-            refName: createRef(),
-            refMemo: createRef(),
-          };
-        })
-      );
-    }
+    if (!scenarios || scenarios.length < 1) return;
+
+    setScenarioStates(
+      scenarios.map((item) => ({
+        name: item.name,
+        memo: item.memo,
+        editName: false,
+        editMemo: false,
+        refName: createRef(), // { current: null } 형태로 초기화
+        refMemo: createRef(), // { current: null } 형태로 초기화
+      }))
+    );
+
+    setIsScenarioSelected(Array(scenarios.length).fill(false));
   }, [scenarios]);
 
   let selRowCount = 0;
 
-  for (const rowCur of selected) if (rowCur) selRowCount++;
+  for (const rowCur of isScenarioSelected) if (rowCur) selRowCount++;
 
-  const handleRowChange = (index: number, newState: Partial<IScenarioStates>) => {
+  const handleRowChange = (index: number, newState: Partial<ScenarioStates>) => {
     setScenarioStates((prev) => {
       const newStates = [...prev];
       newStates[index] = { ...newStates[index], ...newState };
@@ -170,7 +164,7 @@ const SimulationPage: React.FC = () => {
 
   const onDeleteMulti = () => {
     const selIds: string[] = [];
-    for (let i = 0; i < selected.length; i++) if (selected[i]) selIds.push(scenarios[i].id);
+    for (let i = 0; i < isScenarioSelected.length; i++) if (isScenarioSelected[i]) selIds.push(scenarios[i].id);
     PopupAlert.delete(
       'Are you sure you want to delete that list?\nThe deleted list can be checked in Trash Bin.',
       'Delete',
@@ -187,15 +181,12 @@ const SimulationPage: React.FC = () => {
   return (
     <div className="mx-auto max-w-[1340px] px-[30px] pb-24">
       <TheContentHeader text="Simulation" />
+
       <div className="mt-[30px] flex justify-between">
         <h2 className="title-sm">Scenario List</h2>
         <div className="flex items-center gap-[10px]">
-          <Button
-            className="btn-md btn-default"
-            icon={<ListFilterIcon className="size-5" />}
-            text="More Fliters"
-            onClick={() => {}}
-          />
+          {/* <Button className="btn-md btn-default" icon={<ListFilterIcon className="size-5" />} text="More Fliters" onClick={() => {}} /> */}
+
           <Button
             className="btn-md btn-primary"
             icon={<Image width={20} height={20} src="/image/ico-plus.svg" alt="" />}
@@ -210,16 +201,13 @@ const SimulationPage: React.FC = () => {
           />
         </div>
       </div>
-      <div className="mt-[20px] flex items-center justify-between gap-[10px]">
+
+      <div className="mt-4 flex items-center justify-between">
         <div className="tag-block flex flex-grow">
           {/* {visibleDiv === 'tag' && (
-            <div className="pb-[7px] pt-[7px] flex flex-grow items-center gap-[10px] p-[10px]">
-              <button className="tag-sm">
-                ICN <Image width={16} height={16} src="/image/ico-delect.svg" alt="" />
-              </button>
-              <button className="tag-sm">
-                Terminal 1 <Image width={16} height={16} src="/image/ico-delect.svg" alt="" />
-              </button>
+            <div className="flex flex-grow items-center gap-[10px] p-[10px] pb-[7px] pt-[7px]">
+              <button className="tag-sm">ICN <Image width={16} height={16} src="/image/ico-delect.svg" alt="" /></button>
+              <button className="tag-sm">Terminal 1 <Image width={16} height={16} src="/image/ico-delect.svg" alt="" /></button>
             </div>
           )} */}
 
@@ -233,11 +221,15 @@ const SimulationPage: React.FC = () => {
                 onClick={() => onDeleteMulti()}
               />
             </div>
-          ) : null}
+          ) : (
+            <div className="min-h-11"></div>
+          )}
         </div>
-        <Search value={searchKeyword} onChangeText={(text) => setSearchKeyword(text)} />
+
+        {/* <Search value={searchKeyword} onChangeText={(text) => setSearchKeyword(text)} /> */}
       </div>
-      <div className="table-container mt-[20px]">
+
+      <div className="table-container mt-4">
         <table className="table-default">
           <thead>
             <tr className="border-b">
@@ -249,112 +241,140 @@ const SimulationPage: React.FC = () => {
                   onChange={(e) => {
                     const checked = e.target.checked;
                     setSelectAll(checked);
-                    setSelected(Array(scenarios.length).fill(checked)); // 전체 선택/해제
+                    setIsScenarioSelected(Array(scenarios.length).fill(checked)); // 전체 선택/해제
                   }}
                   className="checkbox text-sm"
                 />
               </th>
               <th className="w-[220px] text-left">
-                <button className="inline-flex items-center gap-[2px]">
+                <span className="inline-flex items-center gap-[2px]">
                   Name
-                  <Image width={16} height={16} src="/image/ico-sort.svg" alt="" />
-                </button>
+                  {/* <Image width={16} height={16} src="/image/ico-sort.svg" alt="" /> */}
+                </span>
               </th>
               <th className="w-[120px] text-center">
-                <button className="ml-auto mr-auto inline-flex items-center gap-[2px]">
+                <span className="ml-auto mr-auto inline-flex items-center gap-[2px]">
                   Terminal
-                  <Image width={16} height={16} src="/image/ico-sort.svg" alt="" />
-                </button>
+                  {/* <Image width={16} height={16} src="/image/ico-sort.svg" alt="" /> */}
+                </span>
               </th>
               <th className="w-[120px] text-left">
-                <button className="inline-flex items-center gap-[2px]">
+                <span className="inline-flex items-center gap-[2px]">
                   Editor
-                  <Image width={16} height={16} src="/image/ico-sort.svg" alt="" />
-                </button>
+                  {/* <Image width={16} height={16} src="/image/ico-sort.svg" alt="" /> */}
+                </span>
               </th>
               <th className="w-[120px] text-left">
-                <button className="inline-flex items-center gap-[2px]">
+                <span className="inline-flex items-center gap-[2px]">
                   Target Date
-                  <Image width={16} height={16} src="/image/ico-sort.svg" alt="" />
-                </button>
+                  {/* <Image width={16} height={16} src="/image/ico-sort.svg" alt="" /> */}
+                </span>
               </th>
               <th className="w-[150px] text-left">
-                <button className="inline-flex items-center gap-[2px]">
+                <span className="inline-flex items-center gap-[2px]">
                   Edit Date
-                  <Image width={16} height={16} src="/image/ico-sort.svg" alt="" />
-                </button>
+                  {/* <Image width={16} height={16} src="/image/ico-sort.svg" alt="" /> */}
+                </span>
               </th>
               <th className="!pl-[20px] text-left">Memo</th>
               <th className="w-[90px]"></th>
             </tr>
           </thead>
-          <tbody>
-            {scenarios?.length > 0 && scenarios?.length == scenarioStates?.length ? (
-              scenarios?.map((item, index) =>
-                searchKeyword?.length > 0 && item.name.indexOf(searchKeyword) < 0 ? null : (
-                  <tr key={index} className={`border-b text-sm ${selected[index] ? 'active' : ''}`}>
+
+          <tbody className="min-h-24">
+            {isScenariosLoading ? (
+              <tr>
+                <td colSpan={7}>
+                  <div className="flex flex-1 flex-col items-center justify-center">
+                    <OrbitProgress color="#32cd32" size="medium" text="" textColor="" />
+                  </div>
+                </td>
+              </tr>
+            ) : scenarios.length > 0 && isScenarioSelected.length > 0 ? (
+              scenarios.map((scenario, idx) =>
+                searchKeyword?.length > 0 && scenario.name.indexOf(searchKeyword) < 0 ? null : (
+                  <tr
+                    key={idx}
+                    className={cn('border-b text-sm hover:bg-default-100', isScenarioSelected[idx] ? 'active' : '')}
+                  >
                     <td className="text-center">
                       <Checkbox
-                        label=""
-                        id={`check-${index}`}
-                        checked={selected[index]}
-                        onChange={() =>
-                          setSelected([...selected.map((_, i) => (i == index ? !selected[index] : selected[i]))])
-                        }
+                        id={`check-${idx}`}
                         className="checkbox text-sm"
+                        label=""
+                        checked={isScenarioSelected[idx]}
+                        onChange={() =>
+                          setIsScenarioSelected([
+                            ...isScenarioSelected.map((_, i) =>
+                              i == idx ? !isScenarioSelected[idx] : isScenarioSelected[i]
+                            ),
+                          ])
+                        }
                       />
                     </td>
 
-                    <td>
+                    {/* ===== Scenario Name ===== */}
+                    <td className="cursor-pointer">
                       <div className="flex items-center gap-[10px]">
                         <div
-                          className={`cursor-pointer`}
                           onClick={() => {
-                            if (!scenarioStates[index].editName) {
-                              router.push(`${pathname}/${item.id}`);
+                            if (!scenarioStates[idx].editName) {
+                              router.push(`${pathname}/${scenario.id}`);
                             }
                           }}
                         >
                           <input
-                            ref={scenarioStates[index].refName}
+                            className="!border-none bg-transparent py-[8px] !text-default-700"
+                            style={{ pointerEvents: scenarioStates[idx].editName ? 'auto' : 'none' }}
+                            ref={scenarioStates[idx].refName}
                             type="text"
                             placeholder=""
-                            value={scenarioStates[index]?.name}
-                            className="!border-none bg-transparent py-[8px] !text-default-700"
-                            onChange={(e) => handleRowChange(index, { name: e.target.value })}
-                            disabled={scenarioStates[index].editName ? false : true}
-                            style={{ pointerEvents: scenarioStates[index].editName ? 'auto' : 'none' }}
-                            onBlur={() => onRenameEnd(index)}
+                            value={scenarioStates[idx]?.name}
+                            disabled={scenarioStates[idx].editName ? false : true}
+                            onBlur={() => onRenameEnd(idx)}
+                            onChange={(e) => handleRowChange(idx, { name: e.target.value })}
                             onKeyDown={(e) => {
-                              if (e.key == 'Enter') onRenameEnd(index);
+                              if (e.key == 'Enter') onRenameEnd(idx);
                             }}
                           />
                         </div>
                       </div>
                     </td>
-                    <td className="text-center">{item.terminal}</td>
-                    <td>{item.editor}</td>
+
+                    {/*  ===== Scenario Terminal ===== */}
+                    <td className="text-center">{scenario.terminal}</td>
+
+                    {/*  ===== Scenario Editor ===== */}
+                    <td>{scenario.editor}</td>
+
+                    {/* ===== Scenario Target Date ===== */}
                     <td>
-                      {item?.target_flight_schedule_date
-                        ? dayjs(item?.target_flight_schedule_date).format('MMM-DD-YYYY')
+                      {scenario?.target_flight_schedule_date
+                        ? dayjs(scenario?.target_flight_schedule_date).format('MMM-DD-YYYY')
                         : null}
                     </td>
-                    <td>{dayjs(item?.updated_at).format('MMM-DD-YYYY hh:mm')}</td>
+
+                    {/* ===== Scenario Edit Date ===== */}
+                    <td>{dayjs(scenario?.updated_at).format('MMM-DD-YYYY hh:mm')}</td>
+
+                    {/* ===== Scenario Memo ===== */}
                     <td>
                       <input
-                        ref={scenarioStates[index].refMemo}
+                        ref={scenarioStates[idx].refMemo}
                         type="text"
                         placeholder=""
-                        value={scenarioStates[index]?.memo}
+                        value={scenarioStates[idx]?.memo}
                         className="!border-none bg-transparent py-[8px] !text-default-700"
-                        onChange={(e) => handleRowChange(index, { memo: e.target.value })}
-                        disabled={scenarioStates[index].editMemo ? false : true}
-                        onBlur={() => onEditMemoEnd(index)}
+                        onChange={(e) => handleRowChange(idx, { memo: e.target.value })}
+                        disabled={scenarioStates[idx].editMemo ? false : true}
+                        onBlur={() => onEditMemoEnd(idx)}
                         onKeyDown={(e) => {
-                          if (e.key == 'Enter') onEditMemoEnd(index);
+                          if (e.key == 'Enter') onEditMemoEnd(idx);
                         }}
                       />
                     </td>
+
+                    {/* ===== Scenario Actions ===== */}
                     <td className="text-right">
                       <DropdownMenu>
                         <DropdownMenuTrigger>
@@ -368,19 +388,19 @@ const SimulationPage: React.FC = () => {
                             Run
                           </DropdownMenuItem>
                           <DropdownMenuSeparator />
-                          <DropdownMenuItem onClick={() => onDuplicate(index)}>
+                          <DropdownMenuItem onClick={() => onDuplicate(idx)}>
                             <Image width={16} height={16} src="/image/ico-duplicate.svg" alt="" />
                             Duplicate
                           </DropdownMenuItem>
-                          <DropdownMenuItem onClick={() => onRename(index)}>
+                          <DropdownMenuItem onClick={() => onRename(idx)}>
                             <Image width={16} height={16} src="/image/ico-rename.svg" alt="" />
                             Rename
                           </DropdownMenuItem>
-                          <DropdownMenuItem onClick={() => onEditMemo(index)}>
+                          <DropdownMenuItem onClick={() => onEditMemo(idx)}>
                             <Image width={16} height={16} src="/image/ico-rename.svg" alt="" />
                             Edit Memo
                           </DropdownMenuItem>
-                          <DropdownMenuItem onClick={() => onSetMaster(index)}>
+                          <DropdownMenuItem onClick={() => onSetMaster(idx)}>
                             <Image width={16} height={16} src="/image/ico-rename.svg" alt="" />
                             Set Master
                           </DropdownMenuItem>
@@ -389,7 +409,7 @@ const SimulationPage: React.FC = () => {
                             Share
                           </DropdownMenuItem>
                           <DropdownMenuSeparator />
-                          <DropdownMenuItem className="text-red" onClick={() => onDelete(index)}>
+                          <DropdownMenuItem className="text-red" onClick={() => onDelete(idx)}>
                             <Trash2 className="size-4" />
                             Delete
                           </DropdownMenuItem>
@@ -401,9 +421,9 @@ const SimulationPage: React.FC = () => {
               )
             ) : (
               <tr>
-                <td colSpan={7}>
+                <td colSpan={8}>
                   <div className="flex flex-1 flex-col items-center justify-center">
-                    <OrbitProgress color="#32cd32" size="medium" text="" textColor="" />
+                    <p className="text-sm text-default-500">No scenarios found.</p>
                   </div>
                 </td>
               </tr>
@@ -411,6 +431,7 @@ const SimulationPage: React.FC = () => {
           </tbody>
         </table>
       </div>
+
       <Paging
         currentPage={page}
         totalPage={Math.ceil(totalCount / PAGE_ROW_COUNT)}
