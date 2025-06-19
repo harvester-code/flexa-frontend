ced5423d2fe1d0946a2a43b5e3441adcaef69667
Vincent <vincent@datamarketing.co.kr>
2025-06-19 09:31:41 +0900

    ♻️ Update no data message in HomeNoData component

--- src/app/(protected)/home/_components/HomeNoData.tsx
+++ src/app/(protected)/home/_components/HomeNoData.tsx
@@ -3,7 +3,7 @@ import { Ban } from 'lucide-react';
 function HomeNoData() {
   return (
     <div className="mt-4 flex min-h-40 items-center justify-center gap-2 rounded-md border border-default-200 text-lg text-default-400">
-      <Ban /> There are some troubles to get data. Try again.
+      <Ban /> There are no data to show. Please run a simulation first.
     </div>
   );
 }
