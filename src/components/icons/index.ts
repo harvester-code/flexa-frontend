7c894a04941d11debcb13a928004cc3b01e29f2f
YI <yibyungha@gmail.com>
2025-08-22 08:14:40 +0900

    🔥 Remove unused icon components

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,29 +1,23 @@
 import NavIcon01 from './NavIcon01';
 import NavIcon02 from './NavIcon02';
-import NavIcon03 from './NavIcon03';
 import NavIcon04 from './NavIcon04';
-import NavIcon05 from './NavIcon05';
 import NavIcon06 from './NavIcon06';
 import PassengerQueue from './PassengerQueue';
 import PassengerThroughput from './PassengerThroughput';
 import RatioIcon01 from './RatioIcon01';
 import RatioIcon02 from './RatioIcon02';
 import RatioIcon03 from './RatioIcon03';
-import SearchIcon from './Search';
 import WaitTime from './WaitTime';
 
 export {
   NavIcon01,
   NavIcon02,
-  NavIcon03,
   NavIcon04,
-  NavIcon05,
   NavIcon06,
   PassengerQueue,
   PassengerThroughput,
   RatioIcon01,
   RatioIcon02,
   RatioIcon03,
-  SearchIcon,
   WaitTime,
 };
