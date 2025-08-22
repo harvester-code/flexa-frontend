cffe08ad4d2d8518be7a293f5bb6a86756959450
YI <yibyungha@gmail.com>
2025-08-22 12:53:59 +0900

    ✨ Add SubmitButton component

---
+++ src/components/ui/SubmitButton.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import * as React from 'react';
+import { type VariantProps } from 'class-variance-authority';
+import { useFormStatus } from 'react-dom';
+import { cn } from '@/lib/utils';
+import { Button, buttonVariants } from './Button';
+
+export interface SubmitButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  pendingText?: string;
+}
+
+const SubmitButton = React.forwardRef<HTMLButtonElement, SubmitButtonProps>(
+  ({ className, variant, size = 'btn-lg', pendingText = 'Submitting...', children, ...props }, ref) => {
+    const { pending } = useFormStatus();
+
+    return (
+      <Button
+        ref={ref}
+        type="submit"
+        variant={variant}
+        size={size}
+        className={className}
+        aria-disabled={pending}
+        {...props}
+      >
+        {pending ? pendingText : children}
+      </Button>
+    );
+  }
+);
+SubmitButton.displayName = 'SubmitButton';
+
+export { SubmitButton };
