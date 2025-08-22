b2b3dd3d4b34976d8926339b9b0e3ae2f7cd8a66
YI <yibyungha@gmail.com>
2025-08-22 12:43:49 +0900

    ✨ Add Supabase client setup for browser and server environments, including middleware for session management and user authentication redirection.

---
+++ src/lib/auth/client.ts
@@ -0,0 +1,18 @@
+import { createBrowserClient } from '@supabase/ssr';
+
+let client: ReturnType<typeof createBrowserClient> | undefined;
+
+function createClient() {
+  if (client) {
+    return client;
+  }
+
+  client = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  );
+
+  return client;
+}
+
+export { createClient };
